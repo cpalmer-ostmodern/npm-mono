78589c7656d0ce9fe38b7190ebdd307c6bf26baa
cpalmer-ostmodern <chris.palmer@ostmodern.co.uk>
2023-02-08 15:26:34 +0000

    navbar header component

--- src/app/[lng]/home/page.js
+++ src/app/[lng]/home/page.js
@@ -1,10 +1,11 @@
 "use client";
-
+import { useState, useEffect } from "react";
 import { useTranslation } from "../../i18n/home";
 import { Footer } from "../components/Footer/client";
-import { useState } from "react";
 
 import {
+  NavBar,
+  NavLinks,
   ImageBanner,
   ArticleGrid,
   ArticleGridWithTitle,
@@ -18,6 +19,32 @@ import {
 } from "../../../data/sample-articles";
 
 export default function Page({ params: { lng } }) {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navLinks = [
+    { name: "موضة", href: "#" },
+    { name: "جمال", href: "#" },
+    { name: "ترفيه وفنون", href: "#" },
+    { name: "لايف ستايل", href: "#" },
+    { name: "أخبار", href: "#" },
+    { name: "فيديو", href: "#" },
+    { name: "بودكاست", href: "#" },
+  ];
+
   const { t } = useTranslation(lng, "client-page");
 
   const [counter, setCounter] = useState(0);
@@ -30,7 +57,42 @@ export default function Page({ params: { lng } }) {
   return (
     <>
       <main>
-        <section>{/* <Header heading={t("h1")} /> */}</section>
+        <section>
+          <header
+            className={`z-30 visible md:hidden fixed w-full bg-white ease-in ${
+              scrollY === 0 ? "h-28" : "h-20"
+            }`}
+          >
+            {/* <div className="fixed">{scrollY}</div> */}
+            <NavBar
+              sSize={"23px"}
+              logoSize={scrollY === 0 ? 56 : 33}
+              logoTxt={scrollY === 0 ? true : false}
+              tw="relative pl-7 pr-5 pt-3 pb-4 md:pt-4 md:px-10 md:pb-5 bg-white lg:px-20 flex justify-between w-100 sticky"
+              twMenuOverlay="-z-10 p-5 bg-gold-500 min-h-screen flex justify-center items-center absolute left-0 top-0 w-full"
+              twSearchOverlay="-z-10 p-5 bg-neutral-400 min-h-screen flex justify-center items-center absolute left-0 top-0 w-full"
+            />
+          </header>
+          <header
+            className={`z-30 collapse fixed md:visible w-full bg-white ease-in ${
+              scrollY === 0 ? "h-30" : "h-28"
+            }`}
+          >
+            {/* <div className="fixed">{scrollY}</div> */}
+            <NavBar
+              sSize={"23px"}
+              logoSize={scrollY === 0 ? 72 : 56}
+              logoTxt
+              tw="relative pl-7 pr-5 pt-3 pb-4 md:pt-4 md:px-10 md:pb-5 bg-white lg:px-20 flex justify-between w-100 sticky"
+              twMenuOverlay="-z-10 p-5 bg-gold-500 min-h-screen flex justify-center items-center absolute left-0 top-0 w-full"
+              twSearchOverlay="-z-10 p-5 bg-neutral-400 min-h-screen flex justify-center items-center absolute left-0 top-0 w-full"
+            />
+            <NavLinks
+              tw="bg-white sm:hidden md:flex -mt-2 pt-2 pb-5 pr-20 pl-20 flex justify-center w-100 flex flex-row content-between space-x-12"
+              links={navLinks}
+            ></NavLinks>
+          </header>
+        </section>
 
         <section>
           <div className="md:h-48 h-[10vh] w-full"></div>
--- src/app/[lng]/layout.js
+++ src/app/[lng]/layout.js
@@ -1,24 +1,17 @@
-import './global.css'
+import "./global.css";
 
-import { dir } from 'i18next'
-import { languages } from '../i18n/settings'
+import { dir } from "i18next";
+import { languages } from "../i18n/settings";
 
 export async function generateStaticParams() {
-  return languages.map((lng) => ({ lng }))
+  return languages.map((lng) => ({ lng }));
 }
 
-export default function RootLayout({
-  children,
-  params: {
-    lng
-  }
-}) {
+export default function RootLayout({ children, params: { lng } }) {
   return (
     <html lang={lng} dir={dir(lng)}>
       <head />
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
