7acf4c6fdbd3e0f36076db3b654f1fb7131a859b
cpalmer-ostmodern <chris.palmer@ostmodern.co.uk>
2023-02-09 10:33:39 +0000

    added the language to the homepage

--- src/app/[lng]/page.js
+++ src/app/[lng]/page.js
@@ -60,7 +60,7 @@ export default function Page({
 
   const { t } = useTranslation(
     lng,
-    'client-page',
+    'home-page',
   );
 
   const [counter, setCounter] =
@@ -123,7 +123,7 @@ export default function Page({
               twSearchOverlay="-z-10 p-5 bg-neutral-400 min-h-screen flex justify-center items-center absolute left-0 top-0 w-full"
             />
             <NavLinks
-              tw="bg-white sm:hidden md:flex -mt-2 pt-2 pb-5 pr-20 pl-20 flex justify-center w-100 flex flex-row content-between space-x-12"
+              tw="bg-white sm:hidden md:flex -mt-2 pt-2 pb-5 pr-20 pl-20 flex justify-center w-100 flex flex-row content-between space-x-12 !font-secondary"
               links={navLinks}
             ></NavLinks>
           </header>
@@ -134,13 +134,14 @@ export default function Page({
         </section>
         <section className="relative">
           <ImageBanner
-            title={imgCaption.title}
-            body={imgCaption.body}
+            title={t('img-title')}
+            body={t('h1')}
+            subText={t('back-to-home')}
             tw="md:-mt-[20px] md:h-[441px] w-auto bg-center flex w-screen mb-24 justify-center"
             twTxtBx="w-auto 2xl:w-full 2xl:mx-0 xl:mx-50 lg:mx-32 md:mt-[64.5vh] md:mx-6 text-base block absolute bg-white text-center pt-[24px] pr-[16px] pb-[24px] pl-[16px] z-10 mt-[26rem] mb-20 border-b-[1px] border-black border-solid"
             twTitle="text-lg mb-4"
             twBody="text-5xl font-primary"
-            desktopImg="https://static.hiamag.com/styles/1680x729/public/2023-01/MAIN%20JAN.jpg?h=bde28bee&itok=0XO87sBJ"
+            desktopImg="https://static.hiamag.com/styles/1680x729/public/2023-01/Mainp_0.jpg?h=bde28bee&itok=PYdejC5C"
             mobileImg="https://static.hiamag.com/styles/375x471/public/2023-01/MAIN%20MOBILE.jpg?h=827069f2&itok=JVtZQfy8"
             media="(min-width: 769px)"
             twImg="md:h-[69vh] w-full h-[61.5vh] object-cover object-top"
