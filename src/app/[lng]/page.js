ed8edbf0f23f457dd4a24530f2747b33de759be6
cpalmer-ostmodern <chris.palmer@ostmodern.co.uk>
2023-02-09 22:14:37 +0000

    page layout change

--- src/app/[lng]/page.js
+++ src/app/[lng]/page.js
@@ -137,12 +137,12 @@ export default function Page({
             title={t('img-title')}
             body={t('h1')}
             subText={t('back-to-home')}
-            tw="md:-mt-[20px] md:h-[441px] w-auto bg-center flex w-screen mb-24 justify-center"
-            twTxtBx="w-auto 2xl:w-full 2xl:mx-0 xl:mx-50 lg:mx-32 md:mt-[64.5vh] md:mx-6 text-base block absolute bg-white text-center pt-[24px] pr-[16px] pb-[24px] pl-[16px] z-10 mt-[26rem] mb-20 border-b-[1px] border-black border-solid"
+            tw="md:-mt-[20px] md:h-[441px] bg-center flex w-screen mb-24 justify-center"
+            twTxtBx="w-full 2xl:w-full 2xl:mx-0 md:mt-[64.5vh] text-base block absolute bg-white text-center pt-[24px] pr-[16px] pb-[24px] pl-[16px] z-10 mt-[26rem] mb-20 border-b-[1px] border-black border-solid"
             twTitle="text-lg mb-4"
             twBody="text-5xl font-primary"
             desktopImg="https://static.hiamag.com/styles/1680x729/public/2023-01/Mainp_0.jpg?h=bde28bee&itok=PYdejC5C"
-            mobileImg="https://static.hiamag.com/styles/375x471/public/2023-01/MAIN%20MOBILE.jpg?h=827069f2&itok=JVtZQfy8"
+            mobileImg="https://static.hiamag.com/styles/375x471/public/2023-01/Mainp.jpg?h=bde28bee&itok=BXpokzmn"
             media="(min-width: 769px)"
             twImg="md:h-[69vh] w-full h-[61.5vh] object-cover object-top"
           />
